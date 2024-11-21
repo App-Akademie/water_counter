ab458606f30f8e1b960706bfc74a23eb89969c44
Kai Hatje <kai.hatje@app-akademie.com>
2024-11-21 09:21:43 +0100

    Extracted code to own files.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:water_counter/water_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -52,71 +53,10 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 8.0,
         title: const Text("WaterCounter"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("Anzahl der Getränke"),
-            Text(
-              "$_counter",
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            const SizedBox(height: 64),
-            Padding(
-              padding: EdgeInsets.only(left: 32.0, right: 32.0),
-              child: WCButton(
-                onPressed: _incrementCounter,
-                text: "Trinken",
-              ),
-            ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: EdgeInsets.only(left: 32.0, right: 32.0),
-              child: WCButton(
-                // TODO: Missing function to remove a drink (_decrementCounter).
-                onPressed: null,
-                text: "Getränk entfernen",
-              ),
-            ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: EdgeInsets.only(left: 32.0, right: 32.0),
-              child: WCButton(
-                onPressed: _resetCounter,
-                text: "Zähler zurücksetzen",
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class WCButton extends StatelessWidget {
-  const WCButton({super.key, required this.onPressed, required this.text});
-
-  final VoidCallback? onPressed;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    // Make Button as wide as it can be.
-    return SizedBox(
-      width: double.infinity,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-        onPressed: onPressed,
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.white),
-        ),
+      body: WaterScreen(
+        counter: _counter,
+        incrementCounter: _incrementCounter,
+        resetCounter: _resetCounter,
       ),
     );
   }
