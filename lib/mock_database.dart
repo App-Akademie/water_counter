92e671595a362b8cf2d119b8bcdb90ecb35f54db
Kai Hatje <kai.hatje@app-akademie.com>
2024-11-21 09:34:09 +0100

    Added database repository and mock database.

---
+++ lib/mock_database.dart
@@ -0,0 +1,31 @@
+import 'package:water_counter/database_repository.dart';
+
+class MockDatabase implements DatabaseRepository {
+  int _counter = 0;
+
+  @override
+  Future<int> getCounter() {
+    return Future.value(_counter);
+  }
+
+  @override
+  Future<void> incrementCounter() {
+    _counter++;
+
+    return Future.value();
+  }
+
+  @override
+  Future<void> decrementCounter() {
+    _counter--;
+
+    return Future.value();
+  }
+
+  @override
+  Future<void> resetCounter() {
+    _counter = 0;
+
+    return Future.value();
+  }
+}
